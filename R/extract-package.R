b794e00056785ab79a85b296c3b922049210ee85
Scott Chamberlain <myrmecocystus@gmail.com>
2014-09-21 15:21:40 -0700

    added pdftotext fxn and xpdf function, and package level file

---
+++ R/extract-package.R
@@ -0,0 +1,9 @@
+#' R interface to many web and local pdf extraction tools.
+#'
+#' @name extract-package
+#' @aliases extract
+#' @docType package
+#' @title R client for pdf extraction
+#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
+#' @keywords package
+NULL
---
+++ R/xpdf.r
@@ -0,0 +1,51 @@
+#' PDF-to-text conversion using Rcampdf
+#' 
+#' Uses a local tool called Rcamppdf at \url{http://datacube.wu.ac.at/}.
+#' 
+#' @importFrom tm Corpus URISource readPDF
+#' @importFrom plyr rbind.fill
+#' @export
+#' 
+#' @param files (character) Path to a file, or files on your machine.
+#' @param ... Further args passed on to the \code{tm} package, which does the conversion
+#' 
+#' @author Scott Chamberlain {myrmecocystus@@gmail.com}
+#' @return A S3 object of class xpdf with slots for meta and data
+#' 
+#' @examples \dontrun{
+#' path <- "~/github/sac/scott/pdfs/ChamberlainEtal2013Ecosphere.pdf"
+#' xpdf(files = path)
+#' 
+#' paths <- c("~/github/sac/scott/pdfs/BarraquandEtal2014peerj.pdf", 
+#' "~/github/sac/scott/pdfs/Chamberlain&Holland2009Ecology.pdf",
+#' "~/github/sac/scott/pdfs/Revell&Chamberlain2014MEE.pdf")
+#' (res <- xpdf(files=paths))
+#' res$data[[3]]
+#' }
+
+xpdf <- function(files = NULL, ...)
+{
+  files_exist(files)
+  files <- path.expand(files)
+  out <- Corpus(URISource(files), readerControl=list(reader=readPDF(engine="xpdf", control=list(...))))
+  meta <- get_meta(out)
+  structure(list(meta=meta, data=out), class="xpdf")
+}
+
+#' Use pdftotext to get text from a pdf
+#' @export
+#' @param path A file path.
+#' @param ... Further command line flags passed to pdftotext. see examples
+#' @examples \donttest{
+#' path <- "~/github/sac/scott/pdfs/ChamberlainEtal2013Ecosphere.pdf"
+#' pdftotext(path)
+#' }
+pdftotext <- function(path, ...){
+  cmds <- list(...)
+  cmds <- if(length(cmds)==0) "" else cmds
+  path <- path.expand(path)
+  system(sprintf("pdftotext %s out.txt %s", path, cmds))
+  txt <- readLines('out.txt')
+  txt2 <- gsub("\f", "\n", txt)
+  txt2
+}
