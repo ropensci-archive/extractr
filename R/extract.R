a462f58059046b6362803229a3b21732a485d798
Scott Chamberlain <myrmecocystus@gmail.com>
2015-08-11 14:35:29 -0700

    added example pdf files, fixed up extract() fxn

--- R/extract.R
+++ R/extract.R
@@ -1,22 +1,24 @@
-#' Extract text from a single pdf document.
+#' Extract text from a single pdf document
 #' 
-#' @importFrom Rcampdf pdf_text pdf_info
 #' @export
-#' 
 #' @param path Path to a file
-#' @param which One of rcamp, gs, or xpdf.
+#' @param which One of rcamp, gs, or xpdf (default).
 #' @param ... further args passed on
-#' @return An object of class rcamp_char, gs_char, xpdf_char
+#' @return An object of class rcamp_extr, gs_extr, xpdf_extr, all share the 
+#' same global class extr
 #' @examples \donttest{
-#' res <- extract(path, "rcamp")
-#' res
-#' res <- extract(path, "gs")
-#' res
-#' res <- extract(path, "xpdf")
-#' res
+#' path <- system.file("examples", "example1.pdf", package = "extractr")
+#' xpdf <- extract(path, "xpdf")
+#' xpdf$meta
+#' xpdf$data
+#' gs <- extract(path, "gs")
+#' gs$meta
+#' gs$data
+#' rcamp <- extract(path, "rcamp")
+#' rcamp$meta
+#' rcamp$data
 #' }
-
-extract <- function(path, which, ...){
+extract <- function(path, which = "xpdf", ...){
   switch(which, 
          rcamp = extract_rcamp(path, ...),
          gs = extract_gs(path, ...),
@@ -25,36 +27,49 @@ extract <- function(path, which, ...){
 }
 
 extract_rcamp <- function(path, which, ...){
-  cmds <- list(...)
-  cmds <- if(length(cmds)==0) "" else cmds
   path <- path.expand(path)
-  res <- pdf_text(path)
-  meta <- pdf_info(path)
-  structure(list(meta=meta, data=res), class="rcamp_char", path=path)
+  res <- paste(Rcampdf::pdf_text(path), collapse = ", ")
+  meta <- Rcampdf::pdf_info(path)
+  structure(list(meta = meta, data = res), class = c("rcamp_extr", "extr"), path = path)
 }
 
 extract_gs <- function(path, which, ...){
-  cmds <- list(...)
-  cmds <- if(length(cmds)==0) "" else cmds
+  check_gs()
   path <- path.expand(path)
   res <- pdf_text_via_gs(path)
   res <- paste(res, collapse = ", ")
   meta <- pdf_info_via_gs(path)
-  structure(list(meta=meta, data=res), class="gs_char", path=path)
+  structure(list(meta = meta, data = res), class = c("gs_extr", "extr"), path = path)
 }
 
 extract_xpdf <- function(path, which, ...){
-  cmds <- list(...)
-  cmds <- if(length(cmds)==0) "" else cmds
+  check_pdftotext()
+  cmds <- get_cmds(...)
   path <- path.expand(path)
-  res <- system2("pdftotext", shQuote(path), stdout = TRUE)
-  res <- paste(res, collapse = ", ")
+  res <- system2("pdftotext", paste(cmds, shQuote(path)))
+  newpath <- sub("\\.pdf", ".txt", path)
+  res <- paste(readLines(newpath, warn = FALSE), collapse = ", ")
   meta <- pdf_info_via_xpdf(path)
-  structure(list(meta=meta, data=res), class="xpdf_char", path=path)
+  structure(list(meta = meta, data = res), class = c("xpdf_extr", "extr"), path = path)
+}
+
+get_cmds <- function(...){
+  d <- list(...)
+  if (length(d) == 0) "" else paste0(unlist(d), collapse = " ")
+}
+
+check_pdftotext <- function(x) {
+  chk <- Sys.which("pdftotext")
+  if (chk == "") stop("Please install xpdf. See ?extract_tools for more", call. = FALSE)
+}
+
+check_gs <- function(x) {
+  chk <- Sys.which("gs")
+  if (chk == "") stop("Please install Ghostscript. See ?extract_tools for more", call. = FALSE)
 }
 
 #' @export
-print.rcamp_char <- function(x, ...) {
+print.rcamp_extr <- function(x, ...) {
   cat("<document>", attr(x, "path"), "\n", sep = "")
   cat("  File size: ", x$meta$`File Size`, "\n", sep = "")
   cat("  Pages: ", x$meta$`File Size`, "\n", sep = "")
@@ -63,7 +78,7 @@ print.rcamp_char <- function(x, ...) {
 }
 
 #' @export
-print.gs_char <- function(x, ...) {
+print.gs_extr <- function(x, ...) {
   cat("<document>", attr(x, "path"), "\n", sep = "")
   cat("  Title: ", x$meta$Title, "\n", sep = "")
   cat("  Producer: ", x$meta$Producer, "\n", sep = "")
@@ -71,7 +86,7 @@ print.gs_char <- function(x, ...) {
 }
 
 #' @export
-print.xpdf_char <- function(x, ...) {
+print.xpdf_extr <- function(x, ...) {
   cat("<document>", attr(x, "path"), "\n", sep = "")
   cat("  Pages: ", x$meta$Pages, "\n", sep = "")
   cat("  Title: ", x$meta$Title, "\n", sep = "")
--- R/extractr-package.R
+++ R/extractr-package.R
@@ -1,7 +1,8 @@
 #' @title R client for pdf extraction
 #' 
 #' @description R interface to many web and local pdf extraction tools.
-#'
+#' 
+#' @importFrom Rcampdf pdf_text pdf_info
 #' @name extractr-package
 #' @aliases extractr
 #' @docType package
