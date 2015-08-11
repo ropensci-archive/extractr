0296d84532004c5ebf921ed159188747ebfb780b
Scott Chamberlain <myrmecocystus@gmail.com>
2015-08-11 13:48:11 -0700

    fixed pkg level man file and fixes to make description file happy

--- R/extract-package.R
+++
@@ -1,9 +0,0 @@
-#' R interface to many web and local pdf extraction tools.
-#'
-#' @name extract-package
-#' @aliases extract
-#' @docType package
-#' @title R client for pdf extraction
-#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
-#' @keywords package
-NULL
---
+++ R/extractr-package.R
@@ -0,0 +1,10 @@
+#' @title R client for pdf extraction
+#' 
+#' @description R interface to many web and local pdf extraction tools.
+#'
+#' @name extractr-package
+#' @aliases extractr
+#' @docType package
+#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
+#' @keywords package
+NULL
